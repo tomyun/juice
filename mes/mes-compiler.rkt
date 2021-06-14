b15fa7372163673127b28da668ef0c12adecb8a7
Kyungdahm Yun <tomyun@gmail.com>
2021-06-14 14:07:53 -0700

    Fix shift_jisx0213 iconv handling

--- mes/mes-compiler.rkt
+++ mes/mes-compiler.rkt
@@ -138,14 +138,14 @@
   '())
 
 (define (char->sjis c)
-  (define b8 (string->bytes/utf-8 (string c)))
-  (define t (bytes-open-converter "utf-8" "shift_jisx0213"))
-  ;(define-values (b n r) (bytes-convert t (string->bytes/utf-8 s)))
-  ;FIXME: avoid iconv issue: https://github.com/racket/racket/issues/3876
-  (define b (let-values ([(b n r) (bytes-convert t b8)])
-              (if (zero? (bytes-length b))
-                  (let-values ([(b n r) (bytes-convert t b8)]) b)
-                  b)))
+  ; avoid iconv issue: https://github.com/racket/racket/issues/3876
+  (define s  (string c))
+  (define n8 (string-utf-8-length s))
+  (define b8 (string->bytes/utf-8 s))
+  (define b  (make-bytes 2))
+  (define t  (bytes-open-converter "utf-8" "shift_jisx0213"))
+  (bytes-convert t b8 0 n8 b 0 2)
+  (bytes-convert-end t b)
   (bytes-close-converter t)
   (bytes->list b))
 
