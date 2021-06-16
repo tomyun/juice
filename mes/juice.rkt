c2bff952e0c2e95318aae589d6e85d90f4830163
Kyungdahm Yun <tomyun@gmail.com>
2021-06-16 00:33:37 -0700

    Correct that dict required for both decompile/compile

--- mes/juice.rkt
+++ mes/juice.rkt
@@ -28,7 +28,7 @@
    #:once-each
    [("--force" "-f")     "force overwriting output files"
                          (exists 'replace)]
-   [("--dict") b         "[decompile] dictionary base (80*, D0)"
+   [("--dict") b         "dictionary base (80*, D0)"
                          (cfg:dict (string->number b 16))]
    [("--no-resolve")     "[decompile] skip cmd/sys name resolution"
                          (cfg:resolve #f)]
