5a7a6a0f1ef0e72843a3bbfe9ad325675726580b
Kyungdahm Yun <tomyun@gmail.com>
2021-06-18 11:26:18 -0700

    Use file-value instead of read

--- mes/mes-builder.rkt
+++ mes/mes-builder.rkt
@@ -1,13 +1,13 @@
 #lang racket/base
 
+(require racket/file)
+
 (require "mes-compiler.rkt")
 
 (define-namespace-anchor nsa)
 
 (define (compile-mes path)
-  (define in (open-input-file path))
-  (define src (read in))
-  (close-input-port in)
+  (define src (file->value path))
   (define ns (namespace-anchor->namespace nsa))
   (eval (init) ns)
   (define mes (eval src ns))
