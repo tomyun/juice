4ad78cf3946f3dd4140508910c7b923d58a6c94b
Kyungdahm Yun <tomyun@gmail.com>
2021-06-19 22:51:30 -0700

    Make init

--- bmp/bmp-patcher.rkt
+++ bmp/bmp-patcher.rkt
@@ -16,21 +16,24 @@
     (send src get-argb-pixels x y w h buf)
     (send out set-argb-pixels x y w h buf)))
 
-;; copy half-width glyphs in the first line
-(let ([w  8]
-      [h 16])
-  (blit w
-        h
-        (range 0 W w)
-        (range 0 h h)))
+(define (init)
+  ;; copy half-width glyphs in the first line
+  (let ([w  8]
+        [h 16])
+    (blit w
+          h
+          (range 0 W w)
+          (range 0 h h)))
 
-;; copy full-width glyphs in the remaining lines
-(let ([w 16]
-      [h 16])
-  (blit w
-        h
-        (range 0 W w)
-        (range h H h)))
+  ;; copy full-width glyphs in the remaining lines
+  (let ([w 16]
+        [h 16])
+    (blit w
+          h
+          (range 0 W w)
+          (range h H h))))
+
+(init)
 
 ;; copy extra!
 
